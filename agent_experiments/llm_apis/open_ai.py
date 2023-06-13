fd125f86199e8b8adecceb474ae218a96c73bd6c
ianwu13 <ianwu13@me.com>
2023-06-13 14:43:11 -0700

    now requires model name to be provided

--- agent_experiments/llm_apis/open_ai.py
+++ agent_experiments/llm_apis/open_ai.py
@@ -14,7 +14,7 @@ class OpenAI_Api():
 
     def __init__(
         self, 
-        model_name='gpt-3.5-turbo', 
+        model_name, 
         api_key=None, 
         api_url='https://api.openai.com/v1/completions', 
         prompt_formatter=(lambda x: x), 
