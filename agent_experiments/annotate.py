08fff702589d481075d9787a89c0af80919ce740
Zoraaaa <rongyu923826@outlook.com>
2023-06-13 11:55:36 -0700

    updated i2p_function

--- agent_experiments/annotate.py
+++ agent_experiments/annotate.py
@@ -11,6 +11,7 @@ import argparse
 import pdb
 import re
 import random
+import importlib
 
 import numpy as np
 import torch
@@ -20,53 +21,97 @@ import torch.nn as nn
 
 from data import Annotator
 
+from registry import LLM_API_REG, I2P_REG, DATA_HANDLER_REG
+
+from storage.utilities.open_ai import MY_OPENAI_KEY
+
 
 def main():
     # TODO - START
-    parser = argparse.ArgumentParser(description='selfplaying script')
-    parser.add_argument('--alice_model_file', type=str,
-        help='Alice model file')
-    parser.add_argument('--bob_model_file', type=str,
-        help='Bob model file')
-    parser.add_argument('--context_file', type=str,
-        help='context file')
-    parser.add_argument('--temperature', type=float, default=1.0,
-        help='temperature')
-    parser.add_argument('--verbose', action='store_true', default=False,
-        help='print out converations')
-    parser.add_argument('--seed', type=int, default=1,
-        help='random seed')
-    parser.add_argument('--score_threshold', type=int, default=6,
-        help='successful dialog should have more than score_threshold in score')
-    parser.add_argument('--max_turns', type=int, default=20,
-        help='maximum number of turns in a dialog')
-    parser.add_argument('--log_file', type=str, default='',
-        help='log dialogs to file for training')
-    parser.add_argument('--smart_alice', action='store_true', default=False,
-        help='make Alice smart again')
-    parser.add_argument('--fast_rollout', action='store_true', default=False,
-        help='to use faster rollouts')
-    parser.add_argument('--rollout_bsz', type=int, default=100,
-        help='rollout batch size')
-    parser.add_argument('--rollout_count_threshold', type=int, default=3,
-        help='rollout count threshold')
-    parser.add_argument('--smart_bob', action='store_true', default=False,
-        help='make Bob smart again')
-    parser.add_argument('--ref_text', type=str,
-        help='file with the reference text')
-    parser.add_argument('--domain', type=str, default='object_division',
-        help='domain for the dialogue')
-    args = parser.parse_args()
-
+    parser = argparse.ArgumentParser(description='script to annotate data')
+    # parser.add_argument('--alice_model_file', type=str,
+    #     help='Alice model file')
+    # parser.add_argument('--bob_model_file', type=str,
+    #     help='Bob model file')
+    # parser.add_argument('--context_file', type=str,
+    #     help='context file')
+    # parser.add_argument('--temperature', type=float, default=1.0,
+    #     help='temperature')
+    # parser.add_argument('--verbose', action='store_true', default=False,
+    #     help='print out converations')
+    # parser.add_argument('--seed', type=int, default=1,
+    #     help='random seed')
+    # parser.add_argument('--score_threshold', type=int, default=6,
+    #     help='successful dialog should have more than score_threshold in score')
+    # parser.add_argument('--max_turns', type=int, default=20,
+    #     help='maximum number of turns in a dialog')
+    # parser.add_argument('--log_file', type=str, default='',
+    #     help='log dialogs to file for training')
+    # parser.add_argument('--smart_alice', action='store_true', default=False,
+    #     help='make Alice smart again')
+    # parser.add_argument('--fast_rollout', action='store_true', default=False,
+    #     help='to use faster rollouts')
+    # parser.add_argument('--rollout_bsz', type=int, default=100,
+    #     help='rollout batch size')
+    # parser.add_argument('--rollout_count_threshold', type=int, default=3,
+    #     help='rollout count threshold')
+    # parser.add_argument('--smart_bob', action='store_true', default=False,
+    #     help='make Bob smart again')
+    # parser.add_argument('--ref_text', type=str,
+    #     help='file with the reference text')
+    # parser.add_argument('--domain', type=str, default='object_division',
+    #     help='domain for the dialogue')
+    
     # ARGS IDEAS:
         # Dataset to annotate
         # Destination for annotated data
         # Annotation method (llm or regex parser)
             # if LLM need prompt format template, also which llm to use?
 
-    annotator = Annotator()
+    parser.add_argument('--dataset', type=str, choice = ['DND','CaSiNo'],
+        help='dataset')
+    
+    parser.add_argument('--output_file', type=str,
+        help='destination for output file')
+    
+    parser.add_argument('--inst_to_prompt_funct', type=str, default='func_1',
+        help='function to convert instance to prompt, based on the dataset')
+    
+    parser.add_argument('--annot_method', type=str, default='llm',
+        help='Annotation method (llm or regex parser)')    
+
+    parser.add_argument('--llm_type', type=str, default='falcon',
+        help='llm types')
+
+    args = parser.parse_args()
+
+    print("parsing all done")
+
+    # Get dataset here
+
+    if args.dataset in DATA_HANDLER_REG.keys():
+        DataHandler = importlib.import_module(DATA_HANDLER_REG[args.dataset])
+    else:
+        raise Exception("Dataset name not recgonized")
+    dataset = DataHandler()
+
+    i2p_funct = importlib.import_module(I2P_REG[args.inst_to_prompt_funct])
+
+    if args.annot_method == 'llm':
+        if args.llm_type in LLM_API_REG.keys():
+            AnnotAPI = importlib.import_module(LLM_API_REG[args.llm_type])
+            annot_api = AnnotAPI()
+        else:
+            AnnotAPI = importlib.import_module(LLM_API_REG['openai_generic'])
+            annot_api = AnnotAPI(args.llm_type, MY_OPENAI_KEY)
+    else:
+        raise NotImplementedError("only llm annotation is currently implemented")
+
+    annotator = Annotator(dataset, i2p_funct, annot_api, args.output_file)
 
     annotator.annotate()
+
+    print("annotation all done")
 
 
 if __name__ == '__main__':
---
+++ agent_experiments/data/conversion/i2p_functions.py
@@ -0,0 +1,66 @@
+# assume read file methods are handle outside, and the input for the function is a string.
+
+def extract(str,tag):
+    sub1 = "<" + tag + ">"
+    sub2 = "</"+ tag + ">"
+    
+    idx1 = str.index(sub1)
+    idx2 = str.index(sub2)
+
+    output = str[idx1 + len(sub1) + 1: idx2]
+    return output
+
+
+# split utterance seperately
+def utters_split_seperate(line):
+    line = extract(line, "dialogue")
+    utters_list = []
+    for i in range (0, len(line.split("<eos>"))):
+        each = line.split("<eos>")[i]
+        each = format_prompt(each)
+        utters_list.append(each)
+    return utters_list
+
+def utters_split_cumulative(line):
+    line = extract(line, "dialogue")
+    utters_list = []
+    utters_list.append(format_prompt(line.split("<eos>")[0]))
+    for i in range (1, len(line.split("<eos>"))):
+        temp = line.split("<eos>")[i]
+        each = utters_list[i-1] + temp
+        each = format_prompt(each)
+        utters_list.append(each)
+    return utters_list
+
+def format_prompt(str):
+    prompt_str = "Predict the annotation for the last utterance by following the similar format as provided. "
+    # format the utterance
+    str = "utterance: " + str + "<eos> " + "annotation: "
+    str = prompt_str + str
+    return str
+
+def write_output(prompt_list, output_file):
+    f = open(output_file, 'a')
+    for each in prompt_list:
+        f.write(each + '\n')
+    f.close()
+
+def main():
+    file = open('data.txt', 'r')
+    lines = file.readlines()
+
+    for each_line in lines:
+        each_line = each_line.strip()
+        prompt_sep_list = utters_split_seperate(each_line)
+        write_output(prompt_sep_list,'result_sep.txt')
+
+        prompt_cum_list = utters_split_cumulative(each_line)
+        write_output(prompt_cum_list,'result_cum.txt')
+    print("done")
+        
+
+if __name__ == "__main__":
+    main()
+
+
+    
