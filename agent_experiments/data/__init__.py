c91f0bb0f85905de2bd57d4d813bf8725916f8cb
ianwu13 <ianwu13@me.com>
2023-07-03 19:38:58 -0700

    inst2pfunctions name/import update

--- agent_experiments/data/__init__.py
+++ agent_experiments/data/__init__.py
@@ -1,4 +1,4 @@
-from .conversion import Annotator, i2p_functions, annot2str_functions
+from .conversion import Annotator, inst2p_functions, annot2str_functions
 
 from .casino import CasinoHandler
 from .dealornodeal import DNDHandler
