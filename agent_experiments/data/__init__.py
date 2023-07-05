a68ec64b7e8479943895f6e87377be84b911fb73
ianwu13 <ianwu13@me.com>
2023-07-05 12:13:05 -0700

    reorganized data dir

--- agent_experiments/data/__init__.py
+++ agent_experiments/data/__init__.py
@@ -1,4 +1,6 @@
-from .conversion import Annotator, inst2p_functions, annot2str_functions
+from .conversion import inst2p_functions, annot2str_functions
+
+from .annotator import Annotator
 
 from .casino import CasinoHandler
 from .dealornodeal import DNDHandler
