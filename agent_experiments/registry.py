c4d2846c10e564a6d9d7b668a3791721363ba2c7
ianwu13 <ianwu13@me.com>
2023-07-06 11:43:58 -0700

    added comments

--- agent_experiments/registry.py
+++ agent_experiments/registry.py
@@ -2,11 +2,13 @@
 A registry to define the supported datasets, models, and tasks.
 """
 
+# Data Handlers
 DATA_HANDLER_REG = {
     'dnd': ('data', 'DNDHandler'),
     'casino': ('data', 'CasinoHandler')
 }
 
+# LLM APIs
 LLM_API_REG = {
     "dummy": ("misc.dummies", "DummyModelHandler"),
     "openai_generic": ("lang_models.llm_apis.open_ai", "OpenAI_Api"),
@@ -17,6 +19,7 @@ LLM_API_REG = {
     "gpt_4": ("lang_models.llm_apis.gpt_4", "GPT_4_Api"),
 }
 
+# Functions for Annotation
 INST2ANNOT_PROMPT_FUN_REG = {
     "example": ("data.conversion.inst2p_functions", "example_inst2p_func"),  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
 }
@@ -25,14 +28,17 @@ INST_ANNOT2STR_PROMPT_FUN_REG = {
     "example": ("data.conversion.annot2str_functions", "example_annot2s_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
 }
 
+# Generator Functions (for planning)
 ACT2UTT_PROMPT_FUN_REG = {
     "example": ("lang_models.llm_apis.prompting.act2utt_functions", "example_a2u_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
 }
 
+# Parser Functions (for planning)
 UTT2ACT_PROMPT_FUN_REG = {
     "example": ("lang_models.llm_apis.prompting.utt2act_functions", "example_u2a_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
 }
 
+# Response Generation Functions (dialogue or choice responses)
 RESPONSE_PROMPT_FUN_REG = {
     "example_dia_resp": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_dia_response_func"),  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
     "example_choice": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_choice_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
