48677218ae33c080d7f38cc487de9162cf8c5cc7
ianwu13 <ianwu13@me.com>
2023-07-06 11:54:21 -0700

    Better comments

--- agent_experiments/registry.py
+++ agent_experiments/registry.py
@@ -19,27 +19,33 @@ LLM_API_REG = {
     "gpt_4": ("lang_models.llm_apis.gpt_4", "GPT_4_Api"),
 }
 
-# Functions for Annotation
+# Prompt List to Annotate Dialogue Instance
+# dialogue_instance (format dataset dependent) -> list["prompt_for_annot_single_utt"]
 INST2ANNOT_PROMPT_FUN_REG = {
-    "example": ("data.conversion.inst2p_functions", "example_inst2p_func"),  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
+    "example": ("data.conversion.inst2p_functions", "example_inst2p_func"),
 }
 
+# Dialogue Annotations to Formatted Line Output
+# list["dialogue_act_annotations_for_utterances"] -> "single_line_string_output_for_dialogue"
 INST_ANNOT2STR_PROMPT_FUN_REG = {
-    "example": ("data.conversion.annot2str_functions", "example_annot2s_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
+    "example": ("data.conversion.annot2str_functions", "example_annot2s_func")
 }
 
 # Generator Functions (for planning)
+# dialogue_state_json -> list["prompt_to_gen_dialogue_utt_from_act"]
 ACT2UTT_PROMPT_FUN_REG = {
-    "example": ("lang_models.llm_apis.prompting.act2utt_functions", "example_a2u_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
+    "example": ("lang_models.llm_apis.prompting.act2utt_functions", "example_a2u_func")
 }
 
 # Parser Functions (for planning)
+# dialogue_state_json -> list["prompt_to_parse_dialogue_act"]
 UTT2ACT_PROMPT_FUN_REG = {
-    "example": ("lang_models.llm_apis.prompting.utt2act_functions", "example_u2a_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
+    "example": ("lang_models.llm_apis.prompting.utt2act_functions", "example_u2a_func")
 }
 
 # Response Generation Functions (dialogue or choice responses)
+# dialogue_state_json -> list["prompt_to_gen_response"]
 RESPONSE_PROMPT_FUN_REG = {
-    "example_dia_resp": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_dia_response_func"),  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
-    "example_choice": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_choice_func")  # TODO: COMMENTS EXPLAINING PATTERN IN HERE
+    "example_dia_resp": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_dia_response_func"),
+    "example_choice": ("lang_models.llm_apis.prompting.dia_response_funcitons", "example_choice_func")
 }
