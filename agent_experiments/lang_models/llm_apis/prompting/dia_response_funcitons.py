1705071e14ed96b98e3d0c5185377cd5c2b1d649
ianwu13 <ianwu13@me.com>
2023-07-16 13:14:41 -0700

    removed temp code with return stmnt in it

--- agent_experiments/lang_models/llm_apis/prompting/dia_response_funcitons.py
+++ agent_experiments/lang_models/llm_apis/prompting/dia_response_funcitons.py
@@ -86,13 +86,6 @@ def dia_resp_slagent_openai_chatcomp(inst):
 # CHOICE/SELECTION MAKING PROMPT FUNCTIONS
 
 def choice_slagent_openai_chatcomp(inst):
-    # print(inst['ctx']) -> ['1', '0', '1', '1', '3', '3']
-    # print(inst['dialogue']) -> ['THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY']
-    scenario_str = f"There are {inst['ctx'][0]} books, {inst['ctx'][2]} hats, and {inst['ctx'][4]} balls. The books are worth {inst['ctx'][1]} points, the hats are worth {inst['ctx'][3]}, and the balls are worth {inst['ctx'][5]}. Your partner has different values for each item and you are negotiating to maximize your own points."
-    dialogue_str = f"The negotiation dialogue up to this point is: {' '.join(inst['dialogue'])}"
-    return [f"{scenario_str}\n{dialogue_str}\nThe agreed upon values for the deal, in the format\nyour_books your_hats your_balls their_books their_hats their_balls\n (six numbers), are: "]
-
-
     system_str = f'You are negotiating over books, hats, and balls to maximize your own points. When a participant says "<selection>", this indicates a deal has been reached. In response to this, please give the agreed upon values for the deal, in the format\nyour_books your_hats your_balls their_books their_hats their_balls\n (six numbers).'
     messages = [{"role": "system", "content": system_str}]
     if len(inst['dialogue']) == 0:
