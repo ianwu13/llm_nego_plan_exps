2c18637bb28acc74a9b18e05c4f24464cec63adb
ianwu13 <ianwu13@me.com>
2023-07-19 13:00:59 -0700

    tmp code while setting up dl agent selfplay

--- agent_experiments/lang_models/llm_apis/prompting/dia_response_funcitons.py
+++ agent_experiments/lang_models/llm_apis/prompting/dia_response_funcitons.py
@@ -170,3 +170,26 @@ def choice_slagent_chatcomp_dnd(inst):
         messages.append({"role": "user", "content": '<selection>'})
 
     return [messages]
+
+
+# DIALOGUE ACT LEVEL RESPONSE PROMPT FUNCTIONS
+
+# TODO
+def dia_act_chatcomp(inst):
+    """
+    resp_da_prompt = self.rpf({
+        'ctx': self.ctx,
+        'dialogue': self.dialogue,
+        'dia_acts': self.dialogue_acts
+    })
+    # print(inst['ctx']) -> ['1', '0', '1', '1', '3', '3']
+    # print(inst['dialogue']) -> ['THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY']
+    """
+    scenario_str = f"There are {inst['ctx'][0]} books, {inst['ctx'][2]} hats, and {inst['ctx'][4]} balls. The books are worth {inst['ctx'][1]} points, the hats are worth {inst['ctx'][3]}, and the balls are worth {inst['ctx'][5]}. Your partner has different values for each item and you are negotiating to maximize your own points."
+    if len(inst['dialogue']) == 0:
+        return [f"{scenario_str}\nThe best dialogue strategy to start this negotiation is: "]
+    else:
+        dialogue_str = "" # f"The negotiation dialogue up to this point is: {' '.join(inst['dialogue'])}"
+        # NOTE: Adding nl dialogue here leads model to respond with nl utterance even when instructed to doa  dialogue act
+        da_str = f"The series of dialogue strategy acts used so far is: {' '.join(inst['dia_acts'])}"
+        return [f'{scenario_str}\n{dialogue_str}\n{da_str}\nIf an agreement has been reached, say <selection>\nFollowing this template: "propose book=YOUR_BOOKS_COUNT hat=YOUR_HATS_COUNT ball=YOUR_BALLS_COUNT", the best dialogue strategy act to respond with: ']
--- agent_experiments/lang_models/llm_apis/prompting/utt2act_functions.py
+++ agent_experiments/lang_models/llm_apis/prompting/utt2act_functions.py
@@ -3,6 +3,8 @@ Functions which convert instances from datasethandler and corresponding annotati
 for raw dataset files
 """
 
+from data.conversion.fs_examples import *
+
 
 # DEMO
 def example_u2a_func(inst):
@@ -24,3 +26,31 @@ def example_u2a_func(inst):
     else:
         dialogue_str = f"The negotiation dialogue up to this point is: {' '.join(inst['dialogue'])}"
         return [f"{scenario_str}\n{dialogue_str}\n{annot_str}"]
+
+
+def dnd_parser_chatcomp(inst):
+    """
+    # Get dialogue act with parser
+    parser_prompt = self.p_prompt_func({
+        'ctx': self.ctx,
+        'dialogue': self.dialogue,
+        'read_inpt': ' '.join(inpt)
+    })
+
+    # print(inst['ctx']) -> ['1', '0', '1', '1', '3', '3']
+    # print(inst['dialogue']) -> ['THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY', 'YOU:', 'DUMMY', 'THEM:', 'DUMMY']
+    """
+    system_msg = {
+        'role': 'system',
+        'content': 'You are a professional annotator assisting the user in annotating utterances in a negotiation dialogue, given the context of how many of each item is available. Possible annotations are: "greet", "inquire:, "propose", "disagree", "insist", and "agree"'
+        }
+    user_fs_msg_str = 'Here are some examples of how I want the annotations to look:\n' + '\n'.join([f'{t}\nANNOTATION: "{a}"' for t, a in zip(dnd_fs_texts, dnd_fs_annots_no_vc)])
+
+    ctx_str = f'CONTEXT: "{inst["ctx"][0]} books {inst["ctx"][1]} hats {inst["ctx"][2]} balls"'
+
+    splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+
+    print(f'What is the annotation for this utterance? {ctx_str} UTTERANCE: "{inst["read_inpt"]}"')
+    
+    return [[system_msg, {'role': 'user', 'content': '\n'.join([user_fs_msg_str, f'What is the annotation for this utterance? {ctx_str} UTTERANCE: "{inst["read_inpt"]}"'])}]]
+
