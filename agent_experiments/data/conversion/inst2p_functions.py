cdb649a209ed2f4e62cbf97353da4f7c4722d829
ianwu13 <ianwu13@me.com>
2023-07-28 11:55:02 -0500

    typo fix

--- agent_experiments/data/conversion/inst2p_functions.py
+++ agent_experiments/data/conversion/inst2p_functions.py
@@ -123,7 +123,7 @@ def final_dnd_fs(inst):
 def final_dnd_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in dnd_rb_format.items])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in dnd_rb_format.items()])}, and "Unknown"'
         }
 
     splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
@@ -144,7 +144,7 @@ def final_casino_dnd_form_fs(inst):
 def final_casino_dnd_form_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_dnd_format.items])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_dnd_format.items()])}, and "Unknown"'
         }
 
     splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
@@ -165,7 +165,7 @@ def final_casino_cust_form_fs(inst):
 def final_casino_cust_form_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_cust_format.items])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_cust_format.items()])}, and "Unknown"'
         }
 
     splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
