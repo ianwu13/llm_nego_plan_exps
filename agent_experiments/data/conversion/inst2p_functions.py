00e299f8fe633c1b6c85c1bbc271baa132f780ac
ianwu13 <ianwu13@me.com>
2023-07-28 19:00:09 -0500

    added remove prefix instead of lstrip

--- agent_experiments/data/conversion/inst2p_functions.py
+++ agent_experiments/data/conversion/inst2p_functions.py
@@ -4,6 +4,8 @@ Functions which convert instances from dataset handlers into a set of annotation
 
 from data.conversion.fs_examples import *
 
+from simple_utils import remove_prefix
+
 
 def example_inst2p_func(inst):  # Just for casino dataset, returns "annotate this: {utterance}"
     # print(inst)
@@ -14,7 +16,7 @@ def example_inst2p_func(inst):  # Just for casino dataset, returns "annotate thi
 def example_inst2p_func_dnd(inst):  # Just for casino dataset, returns "annotate this: {utterance}"
     # print(inst)
     # print(type(inst))
-    return [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+    return [remove_prefix(remove_prefix(u, 'YOU: '), 'THEM: ') for u in inst['dialogue'].split(' <eos> ')]
 
 
 def format_prompt(strg):
@@ -31,7 +33,7 @@ def completion_dnd_annot_prompt_fun(inst):
 
     fs_examples_str = '\n'.join([f'{t}\nANNOTATION: "{a}"' for t, a in zip(dnd_fs_texts, dnd_fs_annots_no_vc)])
     
-    splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+    splt_dia = [remove_prefix(remove_prefix(u, 'YOU: '), 'THEM: ') for u in inst['dialogue'].split(' <eos> ')]
     item_counts = inst['input']['count']
     ctx_str = f'CONTEXT: "{item_counts[0]} books {item_counts[1]} hats {item_counts[2]} balls"'
     
@@ -45,7 +47,7 @@ def chat_dnd_annot_prompt_fun(inst):
         }
     user_fs_msg_str = 'Here are some examples of how I want the annotations to look:\n' + '\n'.join([f'{t}\nANNOTATION: "{a}"' for t, a in zip(dnd_fs_texts, dnd_fs_annots_no_vc)])
 
-    splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+    splt_dia = [remove_prefix(remove_prefix(u, 'YOU: '), 'THEM: ') for u in inst['dialogue'].split(' <eos> ')]
     item_counts = inst['input']['count']
     ctx_str = f'CONTEXT: "{item_counts[0]} books {item_counts[1]} hats {item_counts[2]} balls"'
 
@@ -115,11 +117,11 @@ def utility_eg_formatter(value):
 def final_dnd_fs(inst):
     system_msg = {
         'role': 'system',
-        'content': 'You are a professional annotator assisting the user in annotating utterances in a negotiation dialogue, given the context of how many of each item is available. Possible annotations are: "greet", "inquire:, "propose", "disagree", "insist", and "agree"'
+        'content': 'You are a professional annotator assisting the user in annotating utterances in a negotiation dialogue. Possible annotations are: "greet", "inquire:, "propose", "disagree", "insist", and "agree"'
         }
     user_fs_msg_str = 'Here are some examples of how I want the annotations to look:\n' + '\n'.join([f'{t}\nANNOTATION: "{a}"' for t, a in zip(dnd_fs_examples, dnd_annots)])
 
-    splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+    splt_dia = [remove_prefix(remove_prefix(u, 'YOU: '), 'THEM: ') for u in inst['dialogue'].split(' <eos> ')]
     item_counts = inst['input']['count']
     ctx_str = f'CONTEXT: "{item_counts[0]} books {item_counts[1]} hats {item_counts[2]} balls"'
 
@@ -129,10 +131,10 @@ def final_dnd_fs(inst):
 def final_dnd_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in dnd_rb_format.items()])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotation, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in dnd_rb_format.items()])}, and "Unknown"'
         }
 
-    splt_dia = [u.lstrip('YOU: ').lstrip('THEM: ') for u in inst['dialogue'].split(' <eos> ')]
+    splt_dia = [remove_prefix(remove_prefix(u, 'YOU: '), 'THEM: ') for u in inst['dialogue'].split(' <eos> ')]
 
     return [[system_msg, {'role': 'user', 'content': f'What is the annotation for this utterance? "{u}"'}] for u in splt_dia]
 
@@ -150,7 +152,7 @@ def final_casino_dnd_form_fs(inst):
 def final_casino_dnd_form_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_dnd_format.items()])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue for dividing 3 units each of food, water, and firewood. Respond to user requests succinctly, giving only the annotation, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_dnd_format.items()])}, and "Unknown"'
         }
 
     return [[system_msg, {'role': 'user', 'content': f'What is the annotation for this utterance? "{u["text"]}"'}] for u in inst['chat_logs']]
@@ -159,7 +161,7 @@ def final_casino_dnd_form_example(inst):
 def final_casino_cust_form_fs(inst):
     system_msg = {
         'role': 'system',
-        'content': 'You are a professional annotator assisting the user in annotating utterances in a negotiation dialogue.\nPossible annotations are: "Empathy/Coordination", "Undervalue Partner", "Non-strategic", "Small Talk", "No-need", "Vouch Fairness", "Self/Other Need", "Elicit Preferences"'
+        'content': 'You are assisting the user in annotating utterances in a negotiation for dividing 3 units each of food, water, and firewood.\nPossible annotations are: "Empathy/Coordination", "Undervalue Partner", "Non-strategic", "Small Talk", "No-need", "Vouch Fairness", "Self/Other Need", "Elicit Preferences". A single input (utterance) may have multiple correct annotations.'
         }
     user_fs_msg_str = 'Here are some examples of how I want the annotations to look:\n' + '\n'.join([f'UTTERANCE: "{t}"\nANNOTATION: "{a}"' for t, a in zip(casino_cust_format_examples, casino_cust_format_multilab_annots)])
 
@@ -169,7 +171,7 @@ def final_casino_cust_form_fs(inst):
 def final_casino_cust_form_example(inst):
     system_msg = {
         'role': 'system',
-        'content': f'You are assisting the user in annotating utterances in a negotiation dialogue. Respond to user requests succinctly, giving only the annotatino, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_cust_format.items()])}, and "Unknown"'
+        'content': f'You are assisting the user in annotating utterances in a negotiation for dividing 3 units each of food, water, and firewood. Respond to user requests succinctly, giving only the annotation, without extra words. Possible annotations are: {", ".join([f"{k} {utility_eg_formatter(v)}" for k, v in casino_cust_format.items()])}, and "Unknown". A single input (utterance) may have multiple correct annotations.'
         }
 
     return [[system_msg, {'role': 'user', 'content': f'What is the annotation for this utterance? "{u["text"]}"'}] for u in inst['chat_logs']]
