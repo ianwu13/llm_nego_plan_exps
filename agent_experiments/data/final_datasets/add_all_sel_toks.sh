caf02955644346871c7fd0cf74bb72018b31f4a0
ianwu13 <ianwu13@me.com>
2023-07-31 16:02:01 -0500

    adding selection token to annotated datasets

---
+++ agent_experiments/data/final_datasets/add_all_sel_toks.sh
@@ -0,0 +1,11 @@
+python3 add_sel_tok.py --fpath final_dnd/train.txt
+python3 add_sel_tok.py --fpath final_dnd/val.txt
+python3 add_sel_tok.py --fpath final_dnd/test.txt
+
+python3 add_sel_tok.py --fpath final_casino_dndformat/train.txt
+python3 add_sel_tok.py --fpath final_casino_dndformat/val.txt
+python3 add_sel_tok.py --fpath final_casino_dndformat/test.txt
+
+python3 add_sel_tok.py --fpath final_casino_custformat/train.txt
+python3 add_sel_tok.py --fpath final_casino_custformat/val.txt
+python3 add_sel_tok.py --fpath final_casino_custformat/test.txt
---
+++ agent_experiments/data/final_datasets/add_sel_tok.py
@@ -0,0 +1,32 @@
+from fire import Fire
+
+
+def main(fpath):
+    in_file = open(fpath, 'r')
+    lines = in_file.readlines()
+    in_file.close()
+    out_file = open(fpath, 'w')
+    for line in lines:
+        if '<no_agreement>' in line.split('<output>'):
+            out_file.write(line)
+            continue
+
+        new_line_sp = line.split()
+        dia_end = new_line_sp.index('</dialogue>')
+        last_utt_start = dia_end
+        for i in reversed(range(0, dia_end)):
+            if new_line_sp[i] in ['YOU:', 'THEM:']:
+                last_utt_start = i+1
+                break
+
+        new_line_sp = new_line_sp[:last_utt_start] + ['<select>'] + new_line_sp[dia_end:]
+
+        new_line = ' '.join(new_line_sp)
+
+        out_file.write(new_line + '\n')
+
+    out_file.close()
+
+
+if __name__ == '__main__':
+    Fire(main)
