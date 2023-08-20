8b3195750c2a465c3b807d2410d4375e24e1ad4c
ianwu13 <ianwu13@me.com>
2023-08-20 16:06:24 -0700

    dataset cleaning

---
+++ agent_experiments/data/final_datasets/final_casino_custformat/clean.py
@@ -0,0 +1,172 @@
+from collections import defaultdict
+
+from fire import Fire
+
+
+DND_ITEMS = ['books', 'hats', 'balls']
+DND_LABELS = [
+    'greet',
+    'inquire',
+    'propose',
+    'disagree',
+    'insist',
+    'agree',
+    'unknown',
+]
+
+CASINO_ITEMS = ['food', 'water', 'firewood']
+CUST_LABELS = [
+    'smalltalk',
+    'empathycoordination',
+    'noneed',  # NEED SLOTS
+    'elicitpreference',
+    'undervalue',  # NEED SLOTS
+    'vouchfairness',
+    'expresspreference',  # NEED SLOTS
+    'propose',  # NEED SLOTS
+    'disagree',
+    'agree',
+    'unknown',
+]
+
+
+def hack_iter(listy):
+    for i in listy:
+        yield i
+    yield None
+
+
+def slot_getter(annot_iterator, labels_list, items_list):
+    slots_list = []
+    
+    cur = next(annot_iterator)
+    while cur:
+        valid = False
+        for i in items_list:
+            if cur.startswith(i):
+                slots_list.append(cur)
+                valid = True
+                break
+        if not valid:
+            break
+
+        cur = next(annot_iterator)
+    
+    # make set to remove duplicates
+    slots_list = set(slots_list)
+    
+    # Sort slots order
+    sorted_slots = []
+    for i in items_list:
+        for s in slots_list:
+            if s.startswith(i):
+                sorted_slots.append(s)
+
+    return cur, sorted_slots
+
+
+def main(in_file: str, out_file: str, dataset: str = 'casino'):
+    # Get proper labels
+    if dataset == 'dnd':
+        items = DND_ITEMS
+        labels = DND_LABELS
+        raise NotImplementedError(
+            'This script is currently intended only for CaSiNo custom format cleaning. I have included dataset as an arg here to make room for future expansion if needed')
+    elif dataset == 'casino':
+        items = CASINO_ITEMS
+        labels = CUST_LABELS
+    else:
+        raise Exception('dataset must be "dnd" or "casino"')
+
+    lines = open(in_file, 'r').readlines()
+
+    with open(out_file, 'w') as f:
+        for line in lines:
+            tmp = line.split(' <dialogue> ')
+            prefix = ''.join([tmp[0],' <dialogue> '])
+            tmp = tmp[1].split(' </dialogue> ')
+            suffix = ''.join([' </dialogue> ', tmp[1]])
+            dialogue = tmp[0].\
+                replace('empathy coordination', 'empathycoordination').\
+                replace('no need', 'noneed').\
+                replace('elicit preference', 'elicitpreference').\
+                replace('vouch fairness', 'vouchfairness').\
+                replace('express preference', 'expresspreference')
+
+            dia_splt = dialogue.split(' <eos> ')
+            for i, u in enumerate(dia_splt):
+                u_splt = u.split()
+                assert u_splt[0] in ['YOU:', 'THEM:'], f'Splitting incorrect for this line: "{line}"'
+                role = u_splt.pop(0)
+
+                # Get labels
+                slots_dict = defaultdict(lambda: [False])
+                annot_iter = hack_iter(u_splt)
+
+                cur = next(annot_iter)
+                while cur:
+                    if cur in labels:
+                        slots_dict[cur][0] = True
+
+                        tmp_cur = cur
+                        cur, slots = slot_getter(annot_iter, labels, items)
+                        slots_dict[tmp_cur].extend(slots)
+                    elif cur == '<selection>':
+                        slots_dict = False
+                        break
+                    else:
+                        print(f'Invalid annot found: {cur}. Full utterance annots: "{u}"')
+                        cur = next(annot_iter)
+
+                if slots_dict:
+                    # If label has been found but does not have necessary slots, remove
+                    for slot_needing_label in ['noneed', 'undervalue', 'expresspreference', 'propose']:
+                        if len(slots_dict[slot_needing_label]) < 2:
+                            slots_dict[slot_needing_label][0] = False
+                    # Make sure there are still some labels or set to unknown
+                    has_slots = False
+                    for l in slots_dict:
+                        if l[0]:
+                            has_slots = True
+                            break
+                    if not has_slots:
+                        slots_dict['unknown'][0] = True
+
+                    labels_list = [role]
+                    for l in labels:
+                        if slots_dict[l][0]:
+                            labels_list.append(l)
+
+                            sorted_slots = slots_dict[l][1:]
+                            
+                            # assert len(sorted_slots) <= 3, f'Too many slots for label {l}. Slots: {sorted_slots}\nUtterance:\n{u}'
+                            # if not (len(sorted_slots) <= 3): print(f'Too many slots for label {l}. Slots: {sorted_slots}')
+
+                            labels_list.extend(sorted_slots)
+                else:
+                    labels_list = ['<selection>']
+
+                fixed_annots = ' '.join(labels_list)
+
+                dia_splt[i] = fixed_annots
+
+            # Construct dialgoue string
+            dialogue = ' <eos> '.join(dia_splt)
+
+            # TODO: Try leaving as single tokens. makes more sense for lstm?
+            '''
+            dialogue = dialogue.\
+                replace('empathycoordination', 'empathy coordination').\
+                replace('noneed', 'no need').\
+                replace('elicitpreference', 'elicit preference').\
+                replace('vouchfairness', 'vouch fairness').\
+                replace('expresspreference', 'express preference')
+            '''
+
+            new_line = ''.join([prefix, dialogue, suffix])
+            f.write(new_line)
+
+
+if __name__ == '__main__':
+    Fire(main)
+    print('DONE')
