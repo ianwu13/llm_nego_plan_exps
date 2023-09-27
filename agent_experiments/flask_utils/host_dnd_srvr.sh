0050559c25815a382240de029c24a147107a04d6
Ian Wu <ianwu13@me.com>
2023-09-27 02:27:19 -0700

    changed hosting port

--- agent_experiments/flask_utils/host_dnd_srvr.sh
+++ agent_experiments/flask_utils/host_dnd_srvr.sh
@@ -1,2 +1,2 @@
 cd ..
-waitress-serve --listen=127.0.0.1:5000 --threads=8 --max-request-header-size=1048576 --call flask_entrypoint_dnd:create_app
+waitress-serve --listen=0.0.0.0:20 --threads=8 --max-request-header-size=1048576 --call flask_entrypoint_dnd:create_app
