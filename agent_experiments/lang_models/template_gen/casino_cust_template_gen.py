b78be8e332da6bc12f6d95640f1764acc8e52b6f
Ian Wu <ianwu13@me.com>
2023-10-27 16:31:25 -0700

    template gen functions setup

---
+++ agent_experiments/lang_models/template_gen/casino_cust_template_gen.py
@@ -0,0 +1,28 @@
+def parse_acts(da_str: str) -> list:
+    # returns a list of dialogue acts
+    # for example, 
+    #       parse_acts("greet inquire firewood propose water=3")
+    #             --> ["greet", "inquire firewood", "propose water=3"]
+    
+    # TODO LATER
+    return []
+
+
+# TODO NOW make one of these for each dialogue act label
+def tgen_greet():
+    return "hello"
+
+
+# TODO NOW (make one of these for each dialogue act label ^^^)
+
+
+def gen_rephrase_alone_prompts(da_str: str):
+    # prompts to rephrase each dialogue act alone
+    acts_list = parse_acts(da_str)
+    # TODO LATER
+
+
+def gen_rephrase_prompts(da_str: str):
+    # prompts to rephrase all dialogue acts together at once
+    acts_list = parse_acts(da_str)
+    # TODO LATER
