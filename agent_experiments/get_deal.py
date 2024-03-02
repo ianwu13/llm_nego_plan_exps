0bb0a7cd4221026c280b58fb8be7103d17c6869b
Zoraaaa <rongyu923826@outlook.com>
2024-03-01 17:32:03 -0800

    Update get_deal.py

--- agent_experiments/get_deal.py
+++ agent_experiments/get_deal.py
@@ -5,16 +5,24 @@ import os
 api = OpenAI_Api('gpt-4', api_key="")
 
 def get_final_deal(filename, input_dir, output_dir):
-    count = 0
+    count = 1
     f = open(os.path.join(input_dir, filename),"r")
     conversations = json.load(f)
 
     for each in conversations:
         conv = "".join(each["convo"])
-        each_prompt = [{'role': 'system', 'content': 'You are are assisting the user to detect the final deal of dividing 3 units each of food, water, and firewood in a negotiation diglogue. You should give only six numbers in the order of Alice and Bob.'}, 
-            {'role': 'user', 'content': f'What is final deal for the conversation? Only return number. {conv}'}]
+        each_prompt = [{'role': 'system', 'content': 'You are are assisting the user to detect the final deal of dividing 3 units each of firewood, water, and food in a negotiation diglogue. You should get final deal in this format "Alice: firewood=X water=X food=X Bob: firewood=X water=X food=X" and fill X with corresponding numbers from conversation.'}, 
+            {'role': 'user', 'content': f'What is final deal for the conversation? {conv}'}]
         outs = api.get_chat_completions_out([each_prompt])
-        each['final_deal'] = outs[0]
+        final_deal = outs[0]
+        # print(len(final_deal.split()))
+        if len(each["convo"]) >= 20:
+             each['final_deal'] = "alice firewood=0 water=0 food=0 bob firewood=0 water=0 food=0"
+        else:
+            # print(len(final_deal.split()))
+            each['final_deal'] = final_deal
+        each['Conversation'] = count
+        count += 1
 
     with open(os.path.join(output_dir, filename), "w") as outfile: 
         json.dump(conversations, outfile, indent=4)
